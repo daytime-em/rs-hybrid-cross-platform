c6a04e61c6a114b22f62d3f227c89cfca16276d9
Emily Dixon <edixon@mux.com>
2023-10-09 16:52:57 -0700

    vec_md

--- packages/rustlib/binding_ffi/src/outputs.rs
+++ packages/rustlib/binding_ffi/src/outputs.rs
@@ -25,14 +25,13 @@ pub extern "C" fn free_found_primes(found_primes: FoundPrimesFfi) {
 
 impl FromPrimesResult for FoundPrimesFfi {
     fn from_primes_result(result: &rustlib::PrimesResult) -> Self {
-        let /*mut*/ primes_vec = result.primes.clone();
-        //primes_vec.shrink_to_fit(); // When we free this, we want len & capacity to ==
-        let mut vec_box = ManuallyDrop::new(primes_vec.into_boxed_slice());
+        let primes_vec = result.primes.clone();
+        let mut vec_md = ManuallyDrop::new(primes_vec.into_boxed_slice());
         let exec_time_millis = result.exec_time.subsec_millis() as u64 + result.exec_time.as_secs();
         FoundPrimesFfi {
             prime_count: result.primes_count(),
             exec_time_millis,
-            primes: vec_box.as_mut_ptr(),
+            primes: vec_md.as_mut_ptr(),
             up_to: result.up_to,
         }
     }

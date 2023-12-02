0a8a6dd7459340b5ad2aa966d80384930fbb5ea6
Emily Dixon <edixon@mux.com>
2023-12-02 10:34:13 -0800

    Ok now we go

--- packages/web/rustlib-webpack-ts/tests/app.rs
+++ packages/web/rustlib-webpack-ts/tests/app.rs
@@ -12,14 +12,6 @@ fn rust_test() {
     assert_eq!(1, 1);
 }
 
-
-// This runs a unit test in the browser, so it can use browser APIs.
-#[wasm_bindgen_test]
-fn web_test() {
-    assert_eq!(1, 1);
-}
-
-
 // This runs a unit test in the browser, and in addition it supports asynchronous Future APIs.
 #[wasm_bindgen_test(async)]
 fn async_test() -> impl Future<Item = (), Error = JsValue> {

b176d99e3f445b5df143cde403a4a35b03bba182
Emily Dixon <edixon@mux.com>
2023-12-03 13:23:21 -0800

    figuring it out

--- packages/web/ts-primes/src/App.tsx
+++ packages/web/ts-primes/src/App.tsx
@@ -1,12 +1,69 @@
 import React from "react";
 import "./App.css";
 import { PrimeChart } from "./components/PrimeChart";
-import { calcAndLog } from "./utils/calculate";
+import { calcAndLog, PrimesResult, calculate } from "./utils/calculate";
+
+// ====== Hooks and stuff
+
+type AppState = {
+  inputNum: number | undefined,
+  primeData: PrimesResult | undefined,
+}
+
+type AppStateHook = {
+  state: AppState | undefined,
+  setState: (data: AppState | undefined) => void,
+};
+
+function useAppState(): AppStateHook {
+  const [current, setFn] = React.useState<AppState | undefined>({
+    inputNum: undefined, primeData: undefined
+  });
+  return {
+    state: current,
+    setState: setFn,
+  };
+}
+
+// ====== Components
+
+type ComponentProps = {
+  stateHook: AppStateHook
+};
+
+function LoadingContent() {
+  return (
+    "Calculating... "
+  );
+}
+
+function NumberInput(props: ComponentProps) {
+  return (<></>);
+}
+
+function InstructionChart(props: ComponentProps) {
+  return (<></>);
+}
+
+function ResultChart(props: ComponentProps) {
+  return (<></>);
+}
+
+function MainContent() {
+  const stateHook: AppStateHook = useAppState();
+
+  return (
+    <></>
+  );
+}
 
 function App() {
 
   //todo - remove
   calcAndLog(700);
+
+  // todo - useAppState hook :)
+  // let [ primeData, setPrimeData ] = React.useState(null);
 
   return (
     <div className="App">
@@ -15,7 +72,8 @@ function App() {
       </header>
       <main>
         <div className="App-body">
-          <PrimeChart approxPrimes={[1, 2, 6, 7, 8]} />
+          <MainContent />
+          {/* <PrimeChart approxPrimes={[1, 2, 6, 7, 8]} /> */}
         </div>
       </main>
     </div>

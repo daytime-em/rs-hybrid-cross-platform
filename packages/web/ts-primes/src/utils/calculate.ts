2e24099ee16812708af7796a6fe40711b8eb96dd
Emily Dixon <edixon@mux.com>
2023-12-02 23:10:51 -0800

    now we are getting someplace

--- packages/web/ts-primes/src/utils/calculate.ts
+++ packages/web/ts-primes/src/utils/calculate.ts
@@ -1,5 +1,6 @@
 // import { default as rustlib } from "rustlib";
-import { fastFindPrimes } from "rustlib";
+// import { fastFindPrimes } from "rustlib";
+import { fastFindPrimes } from "rustlib-webpack-ts";
 
 /**
  * 
@@ -9,10 +10,10 @@ export function calcAndLog(n: number) {
   // console.log("rustlib imported as object ", rustlib);
   console.log("rustlib imported as object ", fastFindPrimes);
 
-  const result = fastFindPrimes(n);
+  // const result = fastFindPrimes(n);
   // const result = rustlib.fastFindPrimes(n);
 
   // TODO - To use wasm, we need to enable some stuff in webpack on our create-react-app
 
-  console.log("Primes calculated: ", result);
+  // console.log("Primes calculated: ", result);
 }

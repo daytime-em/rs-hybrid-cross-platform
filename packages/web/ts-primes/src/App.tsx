c1b665e6b357b16de7f6006b9fe526a32904d875
Emily Dixon <edixon@mux.com>
2023-12-03 18:15:30 -0800

    Cool so we are working on that

--- packages/web/ts-primes/src/App.tsx
+++ packages/web/ts-primes/src/App.tsx
@@ -7,23 +7,27 @@ import { stat } from "fs";
 // ====== Hooks and stuff
 
 enum UiState {
-  initial, loading, result
+  initial,
+  loading,
+  result,
 }
 
 type AppState = {
-  uiState: UiState,
-  inputNum: number | undefined,
-  primeData: PrimesResult | undefined,
-}
+  uiState: UiState;
+  inputNum: number | undefined;
+  primeData: PrimesResult | undefined;
+};
 
 type AppStateHook = {
-  state: AppState,
-  setState: (data: AppState) => void,
+  state: AppState;
+  setState: (data: AppState) => void;
 };
 
 function useAppState(): AppStateHook {
   const [current, setFn] = React.useState<AppState>({
-    uiState: UiState.initial, inputNum: undefined, primeData: undefined
+    uiState: UiState.initial,
+    inputNum: undefined,
+    primeData: undefined,
   });
   return {
     state: current,
@@ -34,18 +38,12 @@ function useAppState(): AppStateHook {
 // ====== Components
 
 type ComponentProps = {
-  stateHook: AppStateHook
+  stateHook: AppStateHook;
 };
-
-function LoadingContent() {
-  return (
-    "Calculating... "
-  );
-}
 
 function NumberInput(props: ComponentProps) {
   let appStateHook = props.stateHook;
-  let [input, setInput] = useState<string>(); 
+  let [input, setInput] = useState<string>();
 
   const beginCalculating = (n: number) => {
     calculate(n)
@@ -53,51 +51,41 @@ function NumberInput(props: ComponentProps) {
       .then((result) => {
         console.log("App: calculation result", result);
         if (result) {
-            let newState = Object.assign({}, appStateHook.state);
-            newState.uiState = UiState.result;
-            newState.primeData = result;
-            appStateHook.setState(newState);
+          let newState = Object.assign({}, appStateHook.state);
+          newState.uiState = UiState.result;
+          newState.primeData = result;
+          appStateHook.setState(newState);
         }
       });
   };
 
   return (
-  <div className="App-Input-Container">
-    <p>Enter a number between 1 and like a few hundred million</p>
-    <label>
-      Number: <input 
-        value={input}
-        onChange={ev => setInput(ev.target.value)}
-      />
-    </label>
-    <button
-      onClick={() => {
-        if (input) {
-          let parsedInput = parseInt(input);
-          if (parsedInput && !Number.isNaN(parsedInput)) {
-            // Update the UI with the new state
-            let newState = Object.assign({}, appStateHook.state);
-            newState.uiState = UiState.loading;
-            newState.inputNum = parsedInput;
-            appStateHook.setState(newState);
+    <div className="App-Input-Container">
+      <p>Enter a number between 1 and like a few hundred million</p>
+      <label>
+        Number:{" "}
+        <input value={input} onChange={(ev) => setInput(ev.target.value)} />
+      </label>
+      <button
+        onClick={() => {
+          if (input) {
+            let parsedInput = parseInt(input);
+            if (parsedInput && !Number.isNaN(parsedInput)) {
+              // Update the UI with the new state
+              let newState = Object.assign({}, appStateHook.state);
+              newState.uiState = UiState.loading;
+              newState.inputNum = parsedInput;
+              appStateHook.setState(newState);
 
-            beginCalculating(parsedInput);
+              beginCalculating(parsedInput);
+            }
           }
-        }
-      }} 
-    >
-      Calculate 📊
-    </button>
-  </div>
+        }}
+      >
+        Calculate 📊
+      </button>
+    </div>
   );
-}
-
-function InstructionChart(props: ComponentProps) {
-  return (<></>);
-}
-
-function ResultChart(props: ComponentProps) {
-  return (<></>);
 }
 
 function MainContent() {
@@ -106,35 +94,36 @@ function MainContent() {
 
   // todo - don't need this
   // calculate(100_000_000)
-    // .then((res) => console.log("calculated result", res));
+  // .then((res) => console.log("calculated result", res));
 
   if (currentState.uiState === UiState.result) {
-    const chartItems = currentState.primeData?.approxDensities as number[];
+    const primeData = currentState.primeData as PrimesResult; // guaranteed by contract
+    const chartItems = primeData.approxDensities;
     return (
       <div>
-        <NumberInput stateHook={stateHook}/> 
+        <NumberInput stateHook={stateHook} />
+        <p>There are {primeData.primeCount} prime numbers between 1 and {primeData.requestedUpTo}</p>
         <PrimeChart approxPrimes={chartItems} />
       </div>
     );
   } else if (currentState.uiState === UiState.loading) {
     return (
-    <>
-      <p>Calculating...</p>
-    </>
+      <>
+        <p>Calculating...</p>
+      </>
     );
-  } else /*if (currentState.uiState === UiState.initial)*/ {
+  } /*if (currentState.uiState === UiState.initial)*/ else {
     return (
       <>
-        <NumberInput stateHook={stateHook}/> 
+        <NumberInput stateHook={stateHook} />
       </>
     );
   }
-} 
+}
 
 function App() {
-
   //todo - remove
-  calcAndLog(700);
+  // calcAndLog(700);
 
   // todo - useAppState hook :)
   // let [ primeData, setPrimeData ] = React.useState(null);
@@ -147,7 +136,6 @@ function App() {
       <main>
         <div className="App-body">
           <MainContent />
-          {/* <PrimeChart approxPrimes={[1, 2, 6, 7, 8]} /> */}
         </div>
       </main>
     </div>
